8cb53dfd5eed1857518e44a56194bd60e80eb2b0
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-05 09:19:00 +0000

    CONVERSTION IN JS

---
+++ firstjs/converstion.js
@@ -0,0 +1,24 @@
+// console.log(1<2);
+// console.log(1<=2);
+// console.log(1>3);
+// console.log(1!=2);
+// console.log(1==2);
+
+console.log("2"<3);//IT SHOW THAT TRUE IT CONVERT IT INTO BOOLEN BUT JS SAY DO NOT CAMPEAR THE DIFF DATA TYPE
+console.log(3<"4");//IT SHOW THAT FALSEE
+
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);
+
+//IT SAYB THAT JS CAN NOT GIVE AN APRIDECTABLE ANS
+
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+//=== IT IS STRICT CHECK ALSO CHECK THE DATA TYPE
+
+console.log("2"===3);
+
+
