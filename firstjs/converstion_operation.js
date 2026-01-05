2ac0a545c4681c5d793f1d6deb334ebcda68dca7
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-05 07:26:26 +0000

    CONVERTION OF DATA_TYPE

---
+++ firstjs/converstion_operation.js
@@ -0,0 +1,27 @@
+//let Score = "25fff"//it will be the NAN
+//let Score = "null"//it will be 0
+//let Score = "unefineD"//it will be NAN
+//let Score = "true"//it will be 1
+//let Score = "FALSE"//it will be 0
+let Score = "YASH MAHAJAN"
+console.log(typeof Score);
+
+let VALUE = Number(Score)
+console.log(typeof VALUE);
+
+console.log(VALUE);
+ 
+//let bool = 1;// it will be true
+//let bool = 0;//it will be fAlSe
+//let bool = "YASH"//it will true
+let bool = "";//it will be FALSE
+let new_bool = Boolean(bool);
+console.log(new_bool);
+
+
+let ST = 123
+let String1 = String(ST)
+console.log(String1);
+console.log(typeof String1);
+
+ 
