9418fcd07c80231c64fdf97686f5fb34ddc3b511
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-07 07:19:37 +0000

    STRING_IN_DETAIL

---
+++ firstjs/string.js
@@ -0,0 +1,70 @@
+const Y = "YASH MAHAJAN"
+const repocount = 2
+
+//console.log(Y+repocount ,"vlue");
+
+console.log(`MY NAME IS THE ${Y} AND MY REPOCOUNT IS ${repocount}`);
+
+const new_Y = new String("YASH SUDHIR MAHAJAN")   // IT GIVE AN AN LENGTH, KEY VALUE ,AND MULTI MORE
+
+console.log(new_Y);
+console.log(new_Y[0]);
+console.log(Y[0]);
+
+console.log(new_Y.__proto__);
+
+console.log(new_Y.length);
+console.log(new_Y.toUpperCase);
+console.log(new_Y.charAt(2));
+console.log(new_Y.indexOf("Y"));
+
+console.log(new_Y.substring(1,5))
+console.log(new_Y.slice(-3,5))
+
+let yoho = "   nwknwknn    "
+console.log(yoho);
+console.log(yoho.trim());
+
+const url =  "http://YASH MAHAJAN.COM%292U"
+console.log(url);
+console.log(url.replace("%292U","__"));
+
+
+
+console.log(new_Y.split("-"));
+
+
+/*
+String {'yash sudhir mahajan'}0: 
+"y"1: "a"2: "s"3: "h"4: " "5: "s"6: "u"7: "d"8: "h"9: "i"10: "r"11: " "12: "m"13: "a"14: "h"15: "a"16: "j"17: "a"18: 
+"n"length: 19
+[[Prototype]]: Stringanchor: ƒ 
+anchor()at: ƒ at()big: ƒ
+ big()blink: ƒ
+ blink()bold: ƒ 
+ bold()charAt: ƒ 
+ charAt()charCodeAt: ƒ
+  charCodeAt()codePointAt: ƒ
+   codePointAt()concat: ƒ
+    concat()constructor: ƒ
+     String()endsWith: ƒ
+      endsWith()fixed: ƒ
+       fixed()fontcolor: ƒ
+        fontcolor()fontsize: ƒ
+         fontsize()includes: ƒ
+          includes()indexOf: ƒ
+           indexOf()isWellFormed: ƒ
+            isWellFormed()italics: ƒ
+             italics()lastIndexOf: ƒ
+              lastIndexOf()length: 0link: ƒ 
+
+
+
+
+*/
+
+
+
+
+
+
