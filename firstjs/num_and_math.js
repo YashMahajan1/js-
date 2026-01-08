2cdf1116f2e99862af3a2138997dedda1fe2ac32
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-08 09:46:50 +0000

    MATH_JS

---
+++ firstjs/num_and_math.js
@@ -0,0 +1,43 @@
+const y = 3333;
+console.log(y);
+
+
+const y1 = new Number(293222);
+console.log(y1);
+
+console.log(y1.toString());
+
+console.log(y1.toString().length);
+console.log(y1.toFixed(2));
+
+
+let new1 = 112.2222
+console.log(new1.toPrecision(4));
+let new2 = 10000000000000000000000;
+console.log(new2.toLocaleString("en-IN"));
+
+
+
+//########################################## MATHS ################################################
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.4));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+let mx = 20
+let min =10
+
+console.log(Math.floor(Math.random() * (mx-min +1))+min);
+
+
+
+
+
+
+
+
+
