87192e923bed38d4a871376cee04e5b5d196ce5d
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-14 06:49:37 +0000

    OBJECT_DEP

---
+++ JS_basics/object_dep.js
@@ -0,0 +1,69 @@
+
+// //const object1 = new Object()
+// //console.log(object1);
+
+// const objXX = {
+// }
+// obj.ID = "1233"
+// obj.NAME = "YASH MAHAJAN"
+// obj.ISLOGIN = false
+// //onsole.log(obj);
+
+//    NAME:{
+//         USERNAME : "YASH SUDHIR MAHAJAN"
+//     }}
+// }
+// //console.log(REGULERUSER.FULLNAME.NAME.USERNAME);
+
+
+
+
+
+
+// const OBJJ = {1:"A",2:"B"}
+// const OBJJJ =  const REGULERUSER = {
+//     EMAIL : "SUM@123",
+//      FULLNAME : {
+//     {3:"A",4:"B"}
+
+
+// //const BOJJJJ = Object.assign(OBJJ,OBJJJ)
+
+// //console.log(BOJJJJ);
+// OBJJK = {...OBJJ,...OBJJJ}
+// //console.log(OBJJK);
+
+
+
+// let ARRAY = [
+//     OBJ1 = {
+//     ID: "1",
+//     PASS : "11111"
+
+// },
+// OBJ2= {
+//     ID: "1",
+//     PASS : "111112"
+// },
+
+// OBJ3 = {
+//     ID: "1",
+//     PASS : "111113"
+// }
+
+// ]
+// //ARRAY[0].PASS
+// //console.log(ARRAY[0].PASS);
+
+
+
+
+
+const REGULERUSER = {
+    ID : "SNDDS ",
+    PASS : "CSKNNDIN"
+
+}
+console.log(Object.keys(REGULERUSER));
+//WHEN WE ACCES KEY OF ALL IT GIVES INN ARRAY FORAMAT
+
