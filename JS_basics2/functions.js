2bd8853634091e9488a7c4c1892e01b61e92b3a4
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-15 17:40:32 +0000

    FUNCTION_IN_JS

---
+++ JS_basics2/functions.js
@@ -0,0 +1,27 @@
+
+//FUNCTION IS BLOCK OF CODE WHICH IS USE TO ACCES THE MULTIPE CODE AT A TIMME
+
+console.log("D");
+console.log("EV");
+console.log("FFSFEC");
+
+//IF I WANT TO REWRITE THIS CODE 100 TIMES THEN I WILL USE FUNCTIION
+
+function MYFUN (){
+
+console.log("D");
+console.log("EV");
+console.log("FFSFEC");
+
+}
+
+MYFUN()
+
+
+
+function ADDTWOVER(NUMM1,NUM2){
+    console.log(NUM2+NUMM1);
+    
+}
+
+ADDTWOVER(21,1212)
