6367f9886f8c4d74b137214fe6297bdb9b1b1da3
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-17 18:18:45 +0000

    HOISTING_IN _JS

---
+++ JS_basics2/mini_hoisting.js
@@ -0,0 +1,52 @@
+ function one(){
+
+let USERNAME ="YASH"
+
+
+
+    function TWO(){
+        
+        let WEB = "YT"
+        console.log(USERNAME);
+        
+
+
+    }
+//    console.log(WEB);
+
+    TWO()
+    
+ }
+ one()
+
+ if(true){
+    let uernme ="YASH"
+
+    if(uernme==="YASH"){
+        const WEB1 = "YTT"
+        console.log(uernme+WEB1);
+        
+    }
+
+    //console.log(WEB1);
+    
+ }
+
+ //##############################INTRESTING########################33
+
+//console.log(ADDONE(4));  WE CAN ACCES IT BEFORE AND AFTER ALSO
+ function ADDONE(NUM){
+     return NUM+1
+ }
+ ADDONE(5)
+ 
+
+
+
+//  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//NEW(5) BUT IN VER   DEC FUNCTION WE CAN NOT ACCES BEFORE
+
+const NEW = function(NUM1){
+return NUM1+2
+ }
+ NEW(5)
