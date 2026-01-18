db44cf01947fa668ad2e825533fdcb1b8b694cee
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-18 18:41:11 +0000

    NARRW_in_jS

---
+++ JS_basics2/arrow.js
@@ -0,0 +1,24 @@
+const USER = {
+    USERNAME: "YASSH",
+    PRISE : 100,
+
+    WELCOME: function(){
+        console.log(`${this.USERNAME},WELCOMME TO WEBSITE`);
+        
+  console.log(this);
+  
+    }
+}
+// USER.WELCOME()
+// USER.USERNAME="RAJU"
+// USER.WELCOME()
+
+//  console.log(this);
+
+
+
+function one(){console.log(this);
+
+
+}
+one()
