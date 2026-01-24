6aa186de238a936d6cd0556bd2bb29d3fd938406
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-24 08:01:49 +0000

    SWITCH_JS

---
+++ control_flow/switch1.js
@@ -0,0 +1,29 @@
+//  switch (key) {
+//     case value:
+        
+//         break;
+ 
+//     default:
+//         break;
+
+
+const month = "MAR"
+switch (month) {
+    case "JAN":
+        console.log('JAN');
+        
+        break;
+    case "FEB":
+        console.log('FEB');
+        
+        break;
+    case "MAR":
+        console.log('MAR');
+        
+        break;
+
+    default:
+        console.log("DEFUALT MATCH");
+        
+        break;
+}
