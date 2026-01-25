1245cacf0ad70085e90ca768424539dad286ff20
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-25 18:06:39 +0000

    BREAK_CONTINUE_IN_JS

---
+++ itration/n.js
@@ -0,0 +1,32 @@
+
+// // let rry = ["yh","ujbiif","kknfn"]
+// // for (let index = 0; index < rry.length; index++) {
+// //     const element = rry[index];
+// //     console.log(element);
+    
+    
+// // }
+// // console.log(rry.length);
+// //KEY WORDS = BRAKE AND CONTINUE
+// for (let index = 1; index <= 20; index++)
+//      {
+//     if(index ==5){
+//         console.log("DETACT THE FIVE NO");
+//         break
+//     }
+    
+//     console.log(`VALE OF INDEX  IS ${index} `);
+    
+    
+// }
+for (let index = 1; index <= 20; index++)
+      {
+     if(index ==5){
+         console.log("DETACT THE FIVE NO");
+         continue;
+     }
+    
+     console.log(`VALE OF INDEX  IS ${index} `);
+    
+    
+ }
