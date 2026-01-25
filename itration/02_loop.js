a8a508846fafd4d4f9d6ea8504710b7121888158
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-25 18:21:20 +0000

    WHILE_DOWHILE_IN_JS

---
+++ itration/02_loop.js
@@ -0,0 +1,28 @@
+//while
+// while (condition) {
+    
+// }
+
+
+// let i =0
+// while (i <=10) {
+//     console.log(`VALE OF INDEXT IS ${i}`);
+//     i = i+2
+    
+// }
+let myrry = ["ffwewf","fwefqw","cvcf"]
+let i1 = 0
+while (i1<myrry.length) {
+    console.log(`VALUE OF ARRY ${myrry[i1]}`);
+    i1=i1+1
+    
+}
+
+//DO WHILE
+
+let H =1
+do {
+    console.log(`H IS ${H}`);
+    H++
+    
+} while (H<10);
