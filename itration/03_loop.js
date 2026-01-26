74487eff0ab4d973d42c72f7af8a7c675ac84aee
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-26 16:58:14 +0000

    FOR_OF_LOOP

---
+++ itration/03_loop.js
@@ -0,0 +1,19 @@
+ //for of
+
+ const rr =[2,2,21,21,121,12233,]
+ for (const nun of rr) {
+    console.log(nun);
+    
+    
+ }
+
+ const  STRING = "FEVVVVVVVVVVFVEVFF"
+ for (const ST of STRING) {
+    console.log(ST);
+    
+    
+ }
+
+ //MAPS
+
+ 
