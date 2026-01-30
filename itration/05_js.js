d1c0eaac2b47fcda9f728deae29eb689f34db008
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-01-30 17:57:35 +0000

    FILTER_IN_JS

---
+++ itration/05_js.js
@@ -0,0 +1,18 @@
+
+ const VAL = ["12","318783718","313"]
+
+//  const b = VAL.forEach( function (ITEM) {
+//     console.log(ITEM);
+//     return ITEM
+// }
+//  )
+// console.log(b);
+
+
+//filter CAN RETRUN THE VALUES
+
+let t = VAL.filter((num)=>  num>5
+
+)
+console.log(t);
+
